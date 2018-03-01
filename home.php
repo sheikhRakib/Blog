366096d689de7a1a776b503235471e96558402ef
Rakibul Islam <24943853+sheikhRakib@users.noreply.github.com>
2018-03-01 17:02:26 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home!!</title>
+	<style type="text/css">
+		body
+		{
+			background-color: #efefef;
+			width:100%;
+			margin:0;
+			padding: 0;
+		}
+		.foot
+		{
+			width: 100%;
+			color: #ffffff;
+			background-color: #282E28;
+			position: fixed;
+			text-align: center;
+			bottom: 0px;
+
+		}
+	</style>
+</head>
+<body>
+	<h1>Home Page</h1>
+
+	<div class="foot"><?php  include "footer.php";?></div>
+</body>
+</html>
